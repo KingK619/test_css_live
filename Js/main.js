cf236d99f8194ad848bfbc454a3a75a5c779e24e
ritik-kansal <ritikkansal5@gmail.com>
2019-10-30 22:23:26 +0530

    recommit

---
+++ Js/main.js
@@ -0,0 +1,17 @@
+(function () {
+	
+	'use strict';
+
+	var setTextAlignment = function(){
+		$("input").click(function(){
+			var property = $(this).val();
+			$(".text-align").css("text-align", property);
+			alert(property);
+		});
+    };
+
+$(function(){
+		
+		setTextAlignment();
+	});
+}());
